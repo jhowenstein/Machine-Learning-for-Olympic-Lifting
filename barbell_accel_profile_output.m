c1db4710d90081e43100e945587e863afee437a3
Jacob Howenstein <jacob.howenstein@gmail.com>
2017-07-31 10:52:09 -0500

    initial commit

---
+++ barbell_accel_profile_output.m
@@ -0,0 +1,372 @@
+clear variables
+
+N_SS = 16;
+N_PS = 0;
+N_SC = 27;
+N_PC = 10;
+
+ON = 1;
+OFF = 0;
+
+fc = 6;
+fs = 90;
+[b,a] = butter(4,fc/(fs/2));
+
+row_counter = 0;
+
+for q = 1:N_SS
+    file = strcat('SS',num2str(q),'.csv');
+
+    data = transpose(csvread(file));
+
+    time = data(1,:);
+    gyro = data(2:4,:);
+    accel = data(5:7,:);
+    N = length(data);
+    n = 1:N;
+
+    Ax_RAW = data(5,:);
+    Ay_RAW = data(6,:);
+    Az_RAW = data(7,:);
+
+    Ax = filtfilt(b,a,Ax_RAW);
+    Ay = filtfilt(b,a,Ay_RAW);
+    Az = filtfilt(b,a,Az_RAW);
+
+    accel_mag = sqrt(Ay.^2 + Az.^2);
+
+    accel_mag = accel_mag - accel_mag(1);
+
+    Gz = data(10,:);
+
+    Vert_Accel = (Az .* cosd(Gz)) - (Ay .* sind(Gz));
+
+    Hor_Accel = (Az .* sind(Gz)) + (Ay .* cosd(Gz));
+
+    %CAL_VERT = 9.81;
+    CAL_VERT = (Vert_Accel(100));
+
+    Vert_Accel = Vert_Accel - CAL_VERT;
+
+    MHA = find(Hor_Accel == max(Hor_Accel));
+
+    for i = 1:N
+        if Vert_Accel(i) > 1.5;
+            RISE_EDGE = i;
+            break
+        end
+    end
+
+    for i = RISE_EDGE:-1:1
+        if Vert_Accel(i) < 0;
+            START = i + 1;
+            break
+        end
+    end
+
+    for i = MHA+1:N
+        if Vert_Accel(i) < 0
+            MBV = i - 1;
+            break
+        end
+    end
+
+    for i = MBV+1:N
+        if Vert_Accel(i) > 0
+            END = i - 1;
+            break
+        end
+    end
+
+    S = START;
+    E = END;
+
+    PULL_FRAMES = 1:(E-S + 1);
+    N_PULL = length(PULL_FRAMES);
+
+    vertAccel = Vert_Accel(S:E);
+    horAccel = Hor_Accel(S:E);
+
+    PULL_INDEX = PULL_FRAMES * (100/N_PULL);
+    INT_INDEX = 1:100;
+
+    VERT_ACCEL_INT = interp1(PULL_INDEX,vertAccel,INT_INDEX,'spline');
+    HOR_ACCEL_INT = interp1(PULL_INDEX,horAccel,INT_INDEX,'spline');
+
+    row_counter = row_counter + 1;
+    output_row = [VERT_ACCEL_INT,HOR_ACCEL_INT];
+    target_row = [1,0,0,0];
+    
+    output_data(row_counter,:) = output_row;
+    target_data(row_counter,:) = target_row;
+    single_target(row_counter,1) = 0;
+    
+end
+
+for q = 1:N_PS
+    file = strcat('PS',num2str(q),'.csv');
+
+    data = transpose(csvread(file));
+
+    time = data(1,:);
+    gyro = data(2:4,:);
+    accel = data(5:7,:);
+    N = length(data);
+    n = 1:N;
+
+    Ax_RAW = data(5,:);
+    Ay_RAW = data(6,:);
+    Az_RAW = data(7,:);
+
+    Ax = filtfilt(b,a,Ax_RAW);
+    Ay = filtfilt(b,a,Ay_RAW);
+    Az = filtfilt(b,a,Az_RAW);
+
+    accel_mag = sqrt(Ay.^2 + Az.^2);
+
+    accel_mag = accel_mag - accel_mag(1);
+
+    Gz = data(10,:);
+
+    Vert_Accel = (Az .* cosd(Gz)) - (Ay .* sind(Gz));
+
+    Hor_Accel = (Az .* sind(Gz)) + (Ay .* cosd(Gz));
+
+    %CAL_VERT = 9.81;
+    CAL_VERT = (Vert_Accel(100));
+
+    Vert_Accel = Vert_Accel - CAL_VERT;
+
+    MHA = find(Hor_Accel == max(Hor_Accel));
+
+    for i = 1:N
+        if Vert_Accel(i) > 1.5;
+            RISE_EDGE = i;
+            break
+        end
+    end
+
+    for i = RISE_EDGE:-1:1
+        if Vert_Accel(i) < 0;
+            START = i + 1;
+            break
+        end
+    end
+
+    for i = MHA+1:N
+        if Vert_Accel(i) < 0
+            MBV = i - 1;
+            break
+        end
+    end
+
+    for i = MBV+1:N
+        if Vert_Accel(i) > 0
+            END = i - 1;
+            break
+        end
+    end
+
+    S = START;
+    E = END;
+
+    PULL_FRAMES = 1:(E-S + 1);
+    N_PULL = length(PULL_FRAMES);
+
+    vertAccel = Vert_Accel(S:E);
+    horAccel = Hor_Accel(S:E);
+
+    PULL_INDEX = PULL_FRAMES * (100/N_PULL);
+    INT_INDEX = 1:100;
+
+    VERT_ACCEL_INT = interp1(PULL_INDEX,vertAccel,INT_INDEX,'spline');
+    HOR_ACCEL_INT = interp1(PULL_INDEX,horAccel,INT_INDEX,'spline');
+
+    row_counter = row_counter + 1;
+    output_row = [VERT_ACCEL_INT,HOR_ACCEL_INT];
+    target_row = [0,1,0,0];
+    
+    output_data(row_counter,:) = output_row;
+    target_data(row_counter,:) = target_row;
+    single_target(row_counter,1) = 1;
+end
+
+for q = 1:N_SC
+    file = strcat('SC',num2str(q),'.csv');
+
+    data = transpose(csvread(file));
+
+    time = data(1,:);
+    gyro = data(2:4,:);
+    accel = data(5:7,:);
+    N = length(data);
+    n = 1:N;
+
+    Ax_RAW = data(5,:);
+    Ay_RAW = data(6,:);
+    Az_RAW = data(7,:);
+
+    Ax = filtfilt(b,a,Ax_RAW);
+    Ay = filtfilt(b,a,Ay_RAW);
+    Az = filtfilt(b,a,Az_RAW);
+
+    accel_mag = sqrt(Ay.^2 + Az.^2);
+
+    accel_mag = accel_mag - accel_mag(1);
+
+    Gz = data(10,:);
+
+    Vert_Accel = (Az .* cosd(Gz)) - (Ay .* sind(Gz));
+
+    Hor_Accel = (Az .* sind(Gz)) + (Ay .* cosd(Gz));
+
+    %CAL_VERT = 9.81;
+    CAL_VERT = (Vert_Accel(100));
+
+    Vert_Accel = Vert_Accel - CAL_VERT;
+
+    MHA = find(Hor_Accel == max(Hor_Accel));
+
+    for i = 1:N
+        if Vert_Accel(i) > 1.5;
+            RISE_EDGE = i;
+            break
+        end
+    end
+
+    for i = RISE_EDGE:-1:1
+        if Vert_Accel(i) < 0;
+            START = i + 1;
+            break
+        end
+    end
+
+    for i = MHA+1:N
+        if Vert_Accel(i) < 0
+            MBV = i - 1;
+            break
+        end
+    end
+
+    for i = MBV+1:N
+        if Vert_Accel(i) > 0
+            END = i - 1;
+            break
+        end
+    end
+
+    S = START;
+    E = END;
+
+    PULL_FRAMES = 1:(E-S + 1);
+    N_PULL = length(PULL_FRAMES);
+
+    vertAccel = Vert_Accel(S:E);
+    horAccel = Hor_Accel(S:E);
+
+    PULL_INDEX = PULL_FRAMES * (100/N_PULL);
+    INT_INDEX = 1:100;
+
+    VERT_ACCEL_INT = interp1(PULL_INDEX,vertAccel,INT_INDEX,'spline');
+    HOR_ACCEL_INT = interp1(PULL_INDEX,horAccel,INT_INDEX,'spline');
+
+    row_counter = row_counter + 1;
+    output_row = [VERT_ACCEL_INT,HOR_ACCEL_INT];
+    target_row = [0,0,1,0];
+    
+    output_data(row_counter,:) = output_row;
+    target_data(row_counter,:) = target_row;
+    single_target(row_counter,1) = 2;
+end
+
+for q = 1:N_PC
+    file = strcat('PC',num2str(q),'.csv');
+
+    data = transpose(csvread(file));
+
+    time = data(1,:);
+    gyro = data(2:4,:);
+    accel = data(5:7,:);
+    N = length(data);
+    n = 1:N;
+
+    Ax_RAW = data(5,:);
+    Ay_RAW = data(6,:);
+    Az_RAW = data(7,:);
+
+    Ax = filtfilt(b,a,Ax_RAW);
+    Ay = filtfilt(b,a,Ay_RAW);
+    Az = filtfilt(b,a,Az_RAW);
+
+    accel_mag = sqrt(Ay.^2 + Az.^2);
+
+    accel_mag = accel_mag - accel_mag(1);
+
+    Gz = data(10,:);
+
+    Vert_Accel = (Az .* cosd(Gz)) - (Ay .* sind(Gz));
+
+    Hor_Accel = (Az .* sind(Gz)) + (Ay .* cosd(Gz));
+
+    %CAL_VERT = 9.81;
+    CAL_VERT = (Vert_Accel(100));
+
+    Vert_Accel = Vert_Accel - CAL_VERT;
+
+    MHA = find(Hor_Accel == max(Hor_Accel));
+
+    for i = 1:N
+        if Vert_Accel(i) > 1.5;
+            RISE_EDGE = i;
+            break
+        end
+    end
+
+    for i = RISE_EDGE:-1:1
+        if Vert_Accel(i) < 0;
+            START = i + 1;
+            break
+        end
+    end
+
+    for i = MHA+1:N
+        if Vert_Accel(i) < 0
+            MBV = i - 1;
+            break
+        end
+    end
+
+    for i = MBV+1:N
+        if Vert_Accel(i) > 0
+            END = i - 1;
+            break
+        end
+    end
+
+    S = START;
+    E = END;
+
+    PULL_FRAMES = 1:(E-S + 1);
+    N_PULL = length(PULL_FRAMES);
+
+    vertAccel = Vert_Accel(S:E);
+    horAccel = Hor_Accel(S:E);
+
+    PULL_INDEX = PULL_FRAMES * (100/N_PULL);
+    INT_INDEX = 1:100;
+
+    VERT_ACCEL_INT = interp1(PULL_INDEX,vertAccel,INT_INDEX,'spline');
+    HOR_ACCEL_INT = interp1(PULL_INDEX,horAccel,INT_INDEX,'spline');
+
+    row_counter = row_counter + 1;
+    output_row = [VERT_ACCEL_INT,HOR_ACCEL_INT];
+    target_row = [0,0,0,1];
+    
+    output_data(row_counter,:) = output_row;
+    target_data(row_counter,:) = target_row;
+    single_target(row_counter,1) = 3;
+end
+
+csvwrite('imu_lift_data.csv',output_data)
+csvwrite('lift_target_data.csv',target_data)
+csvwrite('single_target_data.csv',single_target)
---
+++ lift_identification_modeling.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+data = np.loadtxt('imu_lift_data.csv', delimiter=',')
+targets = np.loadtxt('lift_target_data.csv', delimiter=',')
+single_target = np.loadtxt('single_target_data.csv')
+
+
+avg = np.mean(data, axis=0)
+
+stdev = np.std(data, axis=0)
+
+data_scaled = np.subtract(data, avg)
+
+data_scaled = np.divide(data_scaled, stdev)
+
+#X_train, X_test, y_train, y_test = train_test_split(data_scaled, single_target, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(data, single_target, random_state=0)
+
+lift_SVC = LinearSVC().fit(X_train,y_train)
+print("SVM")
+print("Test set preditions: {}".format(lift_SVC.predict(X_test)))
+print("Test set lift types: {}".format(y_test))
+print("Test set accuracy: {:.2f}".format(lift_SVC.score(X_test, y_test)))
+
+lift_LR = LogisticRegression().fit(X_train,y_train)
+print("Log Regression")
+print("Test set preditions: {}".format(lift_LR.predict(X_test)))
+print("Test set lift types: {}".format(y_test))
+print("Test set accuracy: {:.2f}".format(lift_LR.score(X_test, y_test)))
+
+lift_DT = DecisionTreeClassifier(random_state=0).fit(X_train, y_train)
+print("Decision Tree")
+print("Test set preditions: {}".format(lift_DT.predict(X_test)))
+print("Test set lift types: {}".format(y_test))
+print("Test set accuracy: {:.2f}".format(lift_DT.score(X_test, y_test)))
